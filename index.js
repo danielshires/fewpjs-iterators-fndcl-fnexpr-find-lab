7539612bccb01f7443e28e7ba6ad9a451a7e6b47
danielshires <hello@danielshires.com>
2022-05-09 20:42:22 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+// let find = [1, 3, 5, 6, 8].find(function(e) {
+//     return e % 2 === 0
+// })
 
+// console.log(find)
+
+const record = [{
+        year: "2018",
+        result: "L"
+    },
+    {
+        year: "2017",
+        result: "W"
+    },
+    {
+        year: "2016",
+        result: "N/A"
+    }
+    //...
+]
+
+function superbowlWin(obj) {
+
+    let yearWon = obj.find(function(e) {
+        return e.result === "W"
+    })
+
+    if (yearWon) {
+        return yearWon.year
+    }
+
+}
+
+console.log(superbowlWin(record))
